7bfb02bb31a436ca6b413df501974db2931b6882
mhbappi05 <mhbappi05@gmail.com>
2026-08-14 05:58:58 +0600

    Changes colors/interfaces for Patient Dashboard

--- ecg.php
+++ ecg.php
@@ -130,6 +130,7 @@ if ($records) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/momStylePrevious.css"> -->
     <link rel="stylesheet" href="css/chatbot.css"> <!-- Chatbot CSS -->
     <link rel="stylesheet" href="css/messenger.css"> <!-- messenger CSS -->
     <link rel="stylesheet" href="css/connections.css">
